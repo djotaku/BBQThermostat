34d185975239bb0a3819ca88ae39ae904455cd59
Eric Mesa <ericsbinaryworld@gmail.com>
2019-08-22 18:47:42 -0400

    created the sketch and added the initial comment

---
+++ BBQ_Thermostat/BBQ_Thermostat.ino
@@ -0,0 +1,27 @@
+/* 
+ *  BBQ Thermostat
+ *  
+ *  This design will read the temperature in a smoker and then adjust a fan connected to the air intake to control the temperature. 
+ *  
+ *  V1.0 - will measure the temperature and send the data via MQTT <- currently here
+ *  V1.1 - will receive a target temp via MQTT
+ *  V2.0 - will control the fan
+ *  
+ *  The current circuit:
+ *  
+ *  - Arduino MKR WiFi 1010
+ *  - Arduino MKR THERM Shield attached
+ *  - A K Type thermocouple temperature sensor connected to the shield
+ *  
+ *  Code licensed under GPL V3.0
+ */
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
